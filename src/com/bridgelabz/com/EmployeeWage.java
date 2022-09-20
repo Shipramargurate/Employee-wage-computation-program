f7c5140ddf0cd34776e6ae888fd9736233b6cd76
shipramargurate <shivanikumari22322@gmail.com>
2022-09-21 02:21:22 +0530

    [SHIVANI]: Added Calculate Wages till a condition of total working hours or days is reached

--- src/com/bridgelabz/com/EmployeeWage.java
+++ src/com/bridgelabz/com/EmployeeWage.java
@@ -4,8 +4,10 @@ import java.util.*;
 
 public class EmployeeWage {
 	/*
-	 * Calculating Wages for a Month by assuming 20 working day per month
+	 * Calculate Wages till a condition of total working hours or days is reached
+	 * for a month assuming 100 hours and 20day
 	 */
+
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
@@ -18,25 +20,32 @@ public class EmployeeWage {
 		int monthlySalary = 0;
 		int workingDaysPerMonth = 20;
 		int salary = 0;
+		int day = 0;
+		int monthlyHours = 0;
 
-		for(int i = 0; i < workingDaysPerMonth; i++) // condition for monthly salary
+		while (day != 20 && monthlyHours != 100)// condition till working hours reaches for month
 		{
-		int randomNumber = random.nextInt(3); // will give 0 ,1 and 2 random number
+			day++;
 
-		switch (randomNumber) {
-		case 0:
-			System.out.println("EMPLOYEE IS ABSENT"); // random number will be o then employee is absent
-			break;
-		case 1:
-			salary = fullDayHours * wagePerHours; // if employee is present than his salary will be
-			break;
-		case 2:
-			salary = wagePerHours * halfdayHours; // condition for half day
+			int randomNumber = random.nextInt(3); // will give 0 ,1 and 2 random number
+
+			switch (randomNumber) {
+			case 0:
+				System.out.println("EMPLOYEE IS ABSENT"); // random number will be o then employee is absent
+				break;
+			case 1:
+				salary = fullDayHours * wagePerHours; // if employee is present than his salary will be
+				monthlyHours = monthlyHours + fullDayHours;
+				break;
+			case 2:
+				salary = wagePerHours * halfdayHours; // condition for half day
+				monthlyHours = monthlyHours + halfdayHours;
+			}
+			monthlySalary = monthlySalary + salary;
+			System.out.println("Day " + day + " employee salaray is : " + salary);
 
 		}
-		System.out.println("Total salary of employee is : " + salary);
 
-	}
 		System.out.println("Monthly Salary: " + monthlySalary);
 	}
 }
