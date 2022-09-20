db5a561d6918ae07ab7bf2a16aaf0b36401901a0
shipramargurate <shivanikumari22322@gmail.com>
2022-09-21 01:03:35 +0530

    [SHIVANI]: Added Employee is present or absent program

--- src/com/bridgelabz/com/EmployeeWage.java
+++ src/com/bridgelabz/com/EmployeeWage.java
@@ -1,9 +1,23 @@
 package com.bridgelabz.com;
 
-public class EmployeeWage {
+import java.util.*;
 
+public class EmployeeWage {
+/*
+ * Check Employee is Present or Absent
+ * using random
+ */
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
+
+		Random random = new Random();
+		
+		int randomNumber = random.nextInt(2); // will give 0 or 1 random number
+		
+		if (randomNumber == 1) // condition for present or absent
+			System.out.println("EMPLOYEE IS PRESENT");
+		else
+			System.out.println("EMPLOYEE IS ABSENT");
 	}
 }
