69ec7adf504f99387808ebf9567162d80898d408
shipramargurate <shivanikumari22322@gmail.com>
2022-10-02 14:19:40 +0530

    [shivani] : Added : Refactoring code ussing oops concept

---
+++ src/com/bridgelabz/com/EmployeeWageMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.com;
+
+public class EmployeeWageMain {
+
+	public static void main(String[] args) {
+		EmployeeWage employeewage = new EmployeeWage();
+		Welcome();
+		employeewage.wage();
+	}
+	static void Welcome() {
+	System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
